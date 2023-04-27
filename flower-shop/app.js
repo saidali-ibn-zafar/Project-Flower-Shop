4034a82b0a547623386424074d376e5f71750caa
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-04-27 15:43:08 +0200

    Add files via upload

--- flower-shop/app.js
+++ flower-shop/app.js
@@ -1,19 +1,49 @@
+const catalogHidden = document.querySelector(".hiddenCatalog");
+const contents = document.querySelector(".contents");
+const info = document.querySelector(".info");
+const catalogUnhidden = document.querySelector(".centeredContainer2");
+const discountCard = document.querySelector(".discount-card");
 
 document.getElementById("catalogBtn").addEventListener("click", function () {
-  const gridContents = document.querySelector(".centeredContainer");
-  const contents = document.querySelector(".contents");
-  if (gridContents.classList.contains("hidden")) {
+  if (catalogHidden.classList.contains("hidden")) {
     contents.style.transform = "translateX(-100%)";
-    gridContents.classList.remove("hidden");
-    setTimeout(function () {
-      gridContents.style.bottom = "-10%";
-    }, 250);
-  } else {
-    gridContents.style.bottom = "-100%";
+    catalogHidden.classList.remove("hidden");
+    info.style.display = "none";
+    catalogUnhidden.style.display = "none";
+    discountCard.style.display = "none";
 
     setTimeout(function () {
-      gridContents.classList.add("hidden");
+      catalogHidden.style.bottom = "-10%";
+    }, 250);
+  } else {
+    catalogHidden.style.bottom = "-100%";
+
+    setTimeout(function () {
+      catalogHidden.classList.add("hidden");
       contents.style.transform = "translateX(0)";
+      info.style.display = "block";
+      catalogUnhidden.style.display = "flex";
+      discountCard.style.display = "flex";
     }, 250);
   }
+});
+
+// go top button
+const topLink = document.querySelector(".top-link");
+window.addEventListener("scroll", function () {
+  // console.log(window.pageYOffset);
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 150) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+// going to top
+topLink.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+  });
 });
